99b6cde28e58b4be94ec6d87f379779f3b06b841
hafid ismaili <hafid.ismaili92@gmail.com>
2023-11-25 10:06:58 +0000

    installing postgres and postgres adapter

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+
+"""
+Calculator function
+"""
+
+
+from re import X
+
+
+def add(x,y):
+    """add two mamebers and return result"""
+    return x+y
+
+def subtract(x, y):
+    """subtract x from y"""
+    return y-x
